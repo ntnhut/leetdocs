7d73122667ef083bfb0b36a04d10cc23761549f1
Nhut Nguyen <75016893+ntnhut@users.noreply.github.com>
2023-06-20 13:28:24 +0000

    add logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = "img/logo-transparent.png"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
